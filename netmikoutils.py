4462b2f08ffeb3374adefa8d045308965244421f
Elier Carcases <101164437+cyber-caveman@users.noreply.github.com>
2023-01-29 07:50:48 -0500

    v1.0
    Corrected bugs on netmikoutils.py

--- netmikoutils.py
+++ netmikoutils.py
@@ -1,3 +1,4 @@
+#v1.0
 from netmiko import ConnectHandler
 
 #Describes a device to be used with netmiko
@@ -11,20 +12,27 @@ def def_device(type: str, hostname:str, username:str, password:str):
     return device
 
 #Prints results of command(s) sent to device(s)
-def printCommandRes(device_list: list, command_res_list: list):
-    i=0
-    for device in device_list:
-        print(f"**************{device['host']}***************")
-        print(command_res_list[i])
-        i+=1
+def printCommandRes(device_list: list, command_res_dict: dict):
+    for device, device_results in command_res_dict.items():
+        print(f"**********{device}**********")
+        for command, result in device_results.items():
+            print(f"{command}:\n{result}")
 
-#Sends a list of commands to a list of devices and returns a list with the results
+#Sends a list of commands to a list of devices and returns a dictionary with the results
+#Order of hierarchy is device -> command -> result_of_command.
+#So you can query any result from any command issued to any device in the list like this: result_var['<device>']['<command>']
 def sendCommands(devices: list, commands: list):
-    results = []
+    results = {}
+    
     for device in devices:
         conn = ConnectHandler(**device)
-        results.append(conn.send_config_set(commands)) 
-    return results  #Return a list with results of commands sent to each device
+        device_results = {}
+        for command in commands:
+            res = conn.send_command(command)
+            device_results[command] = res
+        results[device['host']] = device_results
+        conn.disconnect()
+    return results  #Return a dictionary with results of commands sent to each device
 
 #Sends a list of commands to a list of devices, and prints the output
 def sendAndPrint(devices: list, commands: list):
